134fa04ca0e4e966d24b25d083e37c985062ac44
Reid Campolong <reidcampolong@gmail.com>
2019-06-29 18:45:57 -0400

    Added query to API for weather difference

--- signup/management/commands/sendemails.py
+++ signup/management/commands/sendemails.py
@@ -1,25 +1,41 @@
 from django.core.management.base import BaseCommand, CommandError
 from signup.models import Subscriber
 from .privateKey import key
-import requests, json, datetime
+import requests
+import json
+import datetime
+
 
 class Command(BaseCommand):
     help = 'Send emails to registered users'
 
     def handle(self, *args, **options):
-        getWeatherForLocation('Boston')
-        # subscribers = Subscriber.objects.all()
-        # for subscriber in subscribers:
-        #     print("Sending email to " + str(subscriber))
+        subscribers = Subscriber.objects.all()
+        for subscriber in subscribers:
+            print("Temperature difference in " + str(subscriber))
+            print(getTemperatureDifference(subscriber.user_location))
 
 
-baseWeatherURL = 'http://api.weatherbit.io/v2.0/forecast/daily?city=CITY&days=1&key={key}'
+def getTemperatureDifference(location):
+    futureData = getJSONFutureWeather(location)
+    currentData = getJSONCurrentWeather(location)
 
-def getWeatherForLocation(location):
-    response = requests.get(baseWeatherURL.replace('CITY', location)).json()
-    print(response)
-    # response = json_obj
-    # for item in response['list']:
-    #   print(item)
-    # print(len(response['list']))
-    #print(reponse['time'])
+    futureTemp = futureData['data'][0]['temp']
+    currentTemp = currentData['data'][0]['temp']
+    return(float(futureTemp) - float(currentTemp))
+
+
+def getJSONCurrentWeather(location):
+    return requests.get(insertURLParams(_currentWeatherURL, location)).json()
+
+
+def getJSONFutureWeather(location):
+    return requests.get(insertURLParams(_futureWeatherURL, location)).json()
+
+
+def insertURLParams(url, location):
+    return url.replace('CITY', location).replace('KEY', key)
+
+
+_futureWeatherURL = 'http://api.weatherbit.io/v2.0/forecast/daily?city=CITY&days=1&units=I&key=KEY'
+_currentWeatherURL = 'http://api.weatherbit.io/v2.0/current?city=CITY&units=I&key=KEY'
